99b22441b6fcef00a8dc81e139a37fa8135e4713
Larissa Ferrari <larissa.ferrari@unesp.br>
2023-09-25 00:59:39 -0300

    inserção
    Co-authored-by: Jeferson Dietrich <JefDietrich@users.noreply.github.com>
    Co-authored-by: MuAugusto <MuAugusto@users.noreply.github.com>

--- insere_busca_p.cpp
+++ insere_busca_p.cpp
@@ -13,14 +13,14 @@ int main() {
         char dias[4];
     } veic_t;
 	
-	veic_t regs_locs_vei[8] = {	{"11111111111", "ABC1234", "Cliente-1", "Veiculo-11", "2"},
-		                		{"22222222222", "ABC1234", "Cliente-2", "Veiculo-11", "8"},
-		                		{"33333333333", "CDE9874", "Cliente-3", "Veiculo-33", "1"},
-		                		{"44444444444", "ERT4561", "Cliente-4", "Veiculo-44", "11"},
-		                		{"11111111111", "ERT4561", "Cliente-1", "Veiculo-44", "2"},
-		                		{"11111111111", "UJG3574", "Cliente-1", "Veiculo-66", "3"},
-		                		{"77777777777", "TOP5487", "Cliente-7", "Veiculo-22", "2"},
-		                		{"88888888888", "IUY3214", "Cliente-8", "Veiculo-88", "2"}};
+	veic_t regs_locs_vei[8] = {	{"11111111111", "ABC1234", "Cliente-1", "Veiculo-11", "2" },    // testar busca
+		                		{"22222222222", "ABC1234", "Cliente-2", "Veiculo-11", "8" },    // testar busca
+		                		{"33333333333", "CDE9874", "Cliente-3", "Veiculo-33", "1" },    // testar busca
+		                		{"44444444444", "ERT4561", "Cliente-4", "Veiculo-44", "11"},     
+		                		{"11111111111", "ERT4561", "Cliente-1", "Veiculo-44", "2" },    // testar busca
+		                		{"11111111111", "UJG3574", "Cliente-1", "Veiculo-66", "3" },    // testar busca
+		                		{"77777777777", "TOP5487", "Cliente-7", "Veiculo-22", "2" },
+		                		{"88888888888", "IUY3214", "Cliente-8", "Veiculo-88", "2" }};
        
     fd = fopen("insere.bin", "w+b");
     fwrite(regs_locs_vei, sizeof(regs_locs_vei), 1, fd);
--- trabalho2.c
+++ trabalho2.c
@@ -2,6 +2,10 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include "header.h"
+
+char INDICE_ATUALIZADO = '1';
+char INDICE_NAO_ATUALIZADO = '0';
 
 typedef struct
 {
@@ -16,97 +20,61 @@ typedef struct
 {
     char codCliente[12];
     char codVeiculo[8];
-} REMOCAO_VEICULO;
+} CHAVE_PRIMARIA;
 
-void inserirHeader(FILE* arquivoResultado) {
-    rewind(arquivoResultado);
-    int header;
-    if(fread(&header, sizeof(int), 1, arquivoResultado) == 0) {
-        header = -1;
-        fwrite(&header, sizeof(int), 1, arquivoResultado);
-    }
-}
-
-/* Estrutura do registro deletado */
-/*   100         #          304       [ registro ] */
-/* tamanho end.      proximo deletado [ registro ] */
-void inserirRegistro(LOCACAO_VEICULO veiculoAInserir, FILE* arquivoResultado) {
-    char registro[130];
-    sprintf(registro, "%s|%s|%s|%s|%s|", veiculoAInserir.codCliente,
-            veiculoAInserir.codVeiculo,
-            veiculoAInserir.nomeCliente,
-            veiculoAInserir.nomeVeiculo,
-            veiculoAInserir.dia);
-
-    int tamRegistro = strlen(registro), header, posicaoRegistro, tamRegistroDeletado;
-    bool espacoEncontrado = false;
-
-    rewind(arquivoResultado);    
-    fread(&header, sizeof(int), 1, arquivoResultado);
-
-    if(header != -1) {        
-        // Até achar um espaço ou o header for igual a -1
-        while(!espacoEncontrado && header != -1) {            
-            fseek(arquivoResultado, header, SEEK_SET);
-
-            // Guarda posição do registro para inserir depois caso o tamanho seja ideal   
-            posicaoRegistro = ftell(arquivoResultado);
-
-            // Verifica se o tamanho do registro deletado consegue armazenar o registro novo
-            fread(&tamRegistroDeletado, sizeof(int), 1, arquivoResultado);                                                    
-            if (tamRegistroDeletado >= tamRegistro) {   
-                espacoEncontrado = true;                            
-            } 
-
-            // Anda um para pular o # e lê o próximo registro deletado
-            fseek(arquivoResultado, sizeof(char), SEEK_CUR);
-            fread(&header, sizeof(int), 1, arquivoResultado);
-        }     
-    }  
-    
-    if(!espacoEncontrado) {
-        // Se não encontrou um espaço, insere no fim do arquivo
-        fseek(arquivoResultado, 0, SEEK_END);                 
-        fwrite(&tamRegistro, sizeof(int), 1, arquivoResultado);    
-        fwrite(registro, sizeof(char), tamRegistro, arquivoResultado);
-    } else {
-        fseek(arquivoResultado, posicaoRegistro + sizeof(int), SEEK_SET);        
-        // fwrite(&tamRegistro, sizeof(int), 1, arquivoResultado);    
-        fwrite(registro, sizeof(char), tamRegistro, arquivoResultado);
-
-        // Atualiza o header com a posição próximo registro deletado
-        rewind(arquivoResultado);
-        fwrite(&header, sizeof(int), 1, arquivoResultado);
-    } 
-}
-
-int main()
+typedef struct
 {
-    FILE *arquivo = verificaArquivo("insere.bin");
+    CHAVE_PRIMARIA chavePrimaria;
+    int offset;
+} BUSCA_INDICE;
 
+int construirIndice(FILE* indice, FILE* resultado, BUSCA_INDICE dadosIndice[]);
+
+int verificarIndice(FILE* indice, FILE* resultado, BUSCA_INDICE dadosIndice[]);
+
+int inserirRegistro(LOCACAO_VEICULO veiculoAInserir, FILE* arquivoResultado);
+
+void inserirIndice(LOCACAO_VEICULO veiculoAInserir, BUSCA_INDICE dadosIndice[], int offset, int j);
+
+int compareDadosIndice(const void *a, const void *b);
+
+FILE* verificaArquivo(char* arquivo);
+
+int buscaBinaria(FILE *resultado, BUSCA_INDICE dadosIndice[], CHAVE_PRIMARIA chaveBusca, int tamanhoVetor);
+
+void atualizarArquivoIndice(FILE* indice, BUSCA_INDICE dadosIndice[], int j);
+
+int main() {
+    FILE *arquivo = verificaArquivo("insere.bin");
     LOCACAO_VEICULO locacaoVeiculos[8];
     fread(locacaoVeiculos, sizeof(LOCACAO_VEICULO), 8, arquivo);
 
-    arquivo = verificaArquivo("remove.bin");
+    arquivo = verificaArquivo("busca_p.bin");
+    CHAVE_PRIMARIA chavePrimaria[4];
+    fread(chavePrimaria, sizeof(CHAVE_PRIMARIA), 4, arquivo);
     
-    REMOCAO_VEICULO remocaoVeiculo[4];
-    fread(remocaoVeiculo, sizeof(REMOCAO_VEICULO), 4, arquivo);
-
     fclose(arquivo);
 
-    FILE *resultado = fopen("resultado.bin", "a+b");
-    inserirHeader(resultado);
+    FILE *resultado = fopen("resultado.bin", "a+b");    
+    FILE *indice = fopen("indice.bin", "rb");
+    
+    BUSCA_INDICE dadosIndice[8];
+    int j = verificarIndice(indice, resultado, dadosIndice);    
+
     fclose(resultado);
 
-    int opcao, i = 0;
+    if(indice != NULL) {
+        fclose(indice);
+    }
+
+    int opcao, i;
 
     do {
         printf("\n+------------------------------------------+");
         printf("\nSelecione uma das opções abaixo:\n\n");
         printf("(1) Inserir um registro.\n");
-        printf("(2) Remover um registro.\n");
-        printf("(3) Compactar o arquivo.\n");
-        printf("(0) Sair do programa.\n\n");
+        printf("(2) Buscar um registro.\n");
+        printf("(0) Sair do programa.\n");
         printf("\n+------------------------------------------+\n");
         printf("Opção: ");
         scanf("%d", &opcao);
@@ -119,14 +87,22 @@ int main()
                     printf("\nInforme um número de 1 a 8: ");      
                     scanf("%d", &i);
 
-                    if (i == 0) {
+                    if (i == 0) 
                         break;
-                    }else if (i < 1 || i > 8) {
+                    if (i < 1 || i > 8) {
                         printf("Opção inválida!");
+                    } else {
+                        indice = fopen("indice.bin", "r+b");
+                        rewind(indice);
+                        fwrite(&INDICE_NAO_ATUALIZADO, sizeof(char), 1, indice);
+                        fclose(indice);
+
+                        resultado = fopen("resultado.bin", "r+b");
+                        int offSet = inserirRegistro(locacaoVeiculos[i-1], resultado);
+                        fclose(resultado);                                
+                        inserirIndice(locacaoVeiculos[i-1], dadosIndice, offSet, j);
+                        j++;
                     }
-                    resultado = fopen("resultado.bin", "r+b");
-                    inserirRegistro(locacaoVeiculos[i-1], resultado);
-                    fclose(resultado);
                 }
                 break;
             case 2:
@@ -135,25 +111,191 @@ int main()
                     printf("\nInforme um número de 1 a 4: ");      
                     scanf("%d", &i);            
 
-                    if (i == 0) {
+                    if (i == 0)
                         break;
-                    }else if (i < 1 || i > 4) {
+                    if (i < 1 || i > 4) {
                         printf("Opção inválida!");
+                    } else {
+                        resultado = fopen("resultado.bin", "r+b");
+                        int offSet = buscaBinaria(resultado, dadosIndice, chavePrimaria[i-1], j);
+                        fseek(resultado, offSet, SEEK_SET);
+
+                        int tamanhoRegistro;
+                        char registroBuscado[130];
+                        fread(&tamanhoRegistro, sizeof(int), 1, resultado);
+                        fread(registroBuscado, sizeof(char), tamanhoRegistro, resultado);
+
+                        printf("%s\n", registroBuscado);
+                        fclose(resultado);
                     }
-                    resultado = fopen("resultado.bin", "r+b");
-                    removerRegistro(remocaoVeiculo[i-1], resultado);
-                    fclose(resultado);
                 }
-                break;        
-            case 3:                
-                compactarArquivo();
-                printf("\nArquivo compactado com sucesso!\n");
-                break;            
+                break;                  
             case 0:
                 printf("\nSaindo do programa...\n\n");
+                indice = fopen("indice.bin", "r+b");  
+                atualizarArquivoIndice(indice, dadosIndice, j);
+                fclose(indice);
+
                 break;            
             default:
                 break;
         }
     } while (opcao != 0);    
+}
+
+int construirIndice(FILE* indice, FILE* resultado, BUSCA_INDICE dadosIndice[]) {
+    int j = 0, tamanhoRegistro, posicaoRegistro, contadorDeCaninho, i; 
+    char letraLida, codigoLido[20];       
+    
+    rewind(resultado);
+    while(fread(&tamanhoRegistro, sizeof(int), 1, resultado) != 0) { 
+        posicaoRegistro = ftell(resultado);
+        
+        LOCACAO_VEICULO veiculoAInserir;
+        contadorDeCaninho = 0; i = 0;
+        
+        while(true) {                                            
+            if(letraLida == '|') {              
+                contadorDeCaninho++;
+                codigoLido[i] = '\0';  
+                i = 0;
+
+                if(contadorDeCaninho == 1) {
+                    strcpy(veiculoAInserir.codCliente, codigoLido);
+                } else {
+                    strcpy(veiculoAInserir.codVeiculo, codigoLido);
+                    break;
+                }   
+            } else {
+                codigoLido[i] = letraLida;
+                i++;
+            }           
+            fread(&letraLida, sizeof(char), 1, resultado); 
+        }                                   
+
+        inserirIndice(veiculoAInserir, dadosIndice, posicaoRegistro, j);
+        j++;
+    }
+
+    atualizarArquivoIndice(indice, dadosIndice, j);
+    return j;
+}
+
+int verificarIndice(FILE* indice, FILE* resultado, BUSCA_INDICE dadosIndice[]) {
+    if(indice == NULL) {
+        printf("Criando índice...\n");
+        indice = fopen("indice.bin", "a+b");                
+        int qtdIndice = construirIndice(indice, resultado, dadosIndice);
+        fclose(indice);
+        return qtdIndice;
+    }
+
+    rewind(indice);
+    char statusAtualizacao;
+    fread(&statusAtualizacao, sizeof(char), 1, indice);
+
+    if(statusAtualizacao == INDICE_NAO_ATUALIZADO){
+        printf("Índice desatualizado, reconstruindo índice...\n");
+        return construirIndice(indice, resultado, dadosIndice);
+    }
+    int j;
+    BUSCA_INDICE buscaIndice;
+    while (fread(&buscaIndice, sizeof(BUSCA_INDICE), 1, indice) > 0) {        
+        j++;
+    }
+        
+    return j;    
+}
+
+int inserirRegistro(LOCACAO_VEICULO veiculoAInserir, FILE* arquivoResultado) {
+    char registro[130];
+    sprintf(registro, "%s|%s|%s|%s|%s|", veiculoAInserir.codCliente,
+            veiculoAInserir.codVeiculo,
+            veiculoAInserir.nomeCliente,
+            veiculoAInserir.nomeVeiculo,
+            veiculoAInserir.dia);
+
+    int tamRegistro = strlen(registro), header, posicaoRegistro, tamRegistroDeletado;
+    fseek(arquivoResultado, 0, SEEK_END);  
+
+    int offSet = ftell(arquivoResultado);
+
+    fwrite(&tamRegistro, sizeof(int), 1, arquivoResultado);    
+    fwrite(registro, sizeof(char), tamRegistro, arquivoResultado);
+
+    return offSet;
+}
+
+void inserirIndice(LOCACAO_VEICULO veiculoAInserir, BUSCA_INDICE dadosIndice[], int offset, int j) {   
+    BUSCA_INDICE indiceAInserir;
+    indiceAInserir.offset = offset; 
+    
+    CHAVE_PRIMARIA chave;
+    strcpy(chave.codCliente, veiculoAInserir.codCliente);
+    strcpy(chave.codVeiculo, veiculoAInserir.codVeiculo);
+
+    indiceAInserir.chavePrimaria = chave;
+
+    dadosIndice[j] = indiceAInserir;
+    qsort(dadosIndice, (j + 1), sizeof(BUSCA_INDICE), compareDadosIndice);
+}
+
+int compareDadosIndice(const void *a, const void *b) {
+   BUSCA_INDICE *dadoA = (BUSCA_INDICE *)a;
+   BUSCA_INDICE *dadoB = (BUSCA_INDICE *)b;    
+
+    int i = strcmp(dadoA->chavePrimaria.codCliente, dadoB->chavePrimaria.codCliente);
+
+    if(i == 0) {
+        return strcmp(dadoA->chavePrimaria.codVeiculo, dadoB->chavePrimaria.codVeiculo);
+    }
+
+    return i;
+}
+
+FILE* verificaArquivo(char* arquivo) {
+    FILE* fp = fopen(arquivo, "rb");
+    
+    if( (fp == NULL) ) {
+        printf("O arquivo %s não existe.", arquivo);
+        exit(0);
+    }    
+    return fp;
+}
+
+int buscaBinaria(FILE *resultado, BUSCA_INDICE dadosIndice[], CHAVE_PRIMARIA chaveBusca, int tamanhoVetor) {
+    int inicio = 0;
+    int final = tamanhoVetor - 1;
+    int metade;
+    CHAVE_PRIMARIA chaveLida;
+    
+    while(inicio <= final) {
+        metade = (inicio + final) / 2;
+        chaveLida = dadosIndice[metade].chavePrimaria;
+        
+        int encontrado = strcmp(chaveLida.codCliente, chaveBusca.codCliente);
+        if( encontrado == 0 ) {
+            encontrado = strcmp(chaveLida.codVeiculo, chaveBusca.codVeiculo);
+        }
+
+        if( encontrado == 0) {
+            return dadosIndice[metade].offset;
+        } else if( encontrado < 0) {
+            inicio = metade + 1;
+        } else {
+            final = metade - 1;
+        }
+    }
+
+    return -1;
+}
+
+void atualizarArquivoIndice(FILE* indice, BUSCA_INDICE dadosIndice[], int j) {
+    if(j != 0) {
+        fseek(indice, sizeof(char), SEEK_SET);    
+        fwrite(dadosIndice, sizeof(BUSCA_INDICE), j, indice);
+    }    
+    
+    rewind(indice);
+    fwrite(&INDICE_ATUALIZADO, sizeof(char), 1, indice);
 }
